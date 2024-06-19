ea74933010ea007c8fd05558d1f1e8f57f921ed0
Chris Harwood <cmharwood79@gmail.com>
2024-06-19 14:15:44 -0500

    updated user router to add default image when registering

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const {
-  rejectUnauthenticated,
-} = require('../modules/authentication-middleware');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
 const userStrategy = require('../strategies/user.strategy');
@@ -20,17 +18,34 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 router.post('/register', (req, res, next) => {
   const username = req.body.username;
   const password = encryptLib.encryptPassword(req.body.password);
+  const defaultProfileImageUrl =
+    'https://t3.ftcdn.net/jpg/01/18/01/98/360_F_118019822_6CKXP6rXmVhDOzbXZlLqEM2ya4HhYzSV.jpg'; // Set your default image URL here
 
-  const queryText = `INSERT INTO "user" (username, password)
-    VALUES ($1, $2) RETURNING id`;
+  const queryText = `INSERT INTO "user" (username, password, profile_image_url)
+    VALUES ($1, $2, $3) RETURNING id`;
   pool
-    .query(queryText, [username, password])
+    .query(queryText, [username, password, defaultProfileImageUrl])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log('User registration failed: ', err);
       res.sendStatus(500);
     });
 });
+
+// router.post('/register', (req, res, next) => {
+//   const username = req.body.username;
+//   const password = encryptLib.encryptPassword(req.body.password);
+
+//   const queryText = `INSERT INTO "user" (username, password)
+//     VALUES ($1, $2) RETURNING id`;
+//   pool
+//     .query(queryText, [username, password])
+//     .then(() => res.sendStatus(201))
+//     .catch((err) => {
+//       console.log('User registration failed: ', err);
+//       res.sendStatus(500);
+//     });
+// });
 
 // Handles login form authenticate/login POST
 // userStrategy.authenticate('local') is middleware that we run on this route
@@ -44,9 +59,27 @@ router.post('/login', userStrategy.authenticate('local'), (req, res) => {
 router.post('/logout', (req, res, next) => {
   // Use passport's built-in method to log out the user
   req.logout((err) => {
-    if (err) { return next(err); }
+    if (err) {
+      return next(err);
+    }
     res.sendStatus(200);
   });
+});
+
+// Handles updating profile image URL
+router.put('/profile-image', rejectUnauthenticated, (req, res) => {
+  const userId = req.user.id;
+  const { profile_image_url } = req.body;
+
+  const queryText = `UPDATE "user" SET "profile_image_url" = $1 WHERE "id" = $2`;
+
+  pool
+    .query(queryText, [profile_image_url, userId])
+    .then(() => res.sendStatus(200))
+    .catch((err) => {
+      console.log('Error updating profile image:', err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;

0a8c9bfa917492c8271ac5d5f0988fe1931f7185
Chris Harwood <cmharwood79@gmail.com>
2024-06-19 14:21:02 -0500

    declared axios, added export

--- src/redux/sagas/feed.saga.js
+++ src/redux/sagas/feed.saga.js
@@ -1,14 +1,17 @@
-import { takeLatest } from "redux-saga/effects";
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
 
 function* fetchCooks() {
-    try {
-      const response = yield axios.get('/api/feed');
-      yield put({ type: 'SET_COOKS', payload: response.data });
-    } catch (error) {
-      console.error('Error getting cooks');
-    }
+  try {
+    const response = yield axios.get('/api/feed');
+    yield put({ type: 'SET_COOKS', payload: response.data });
+  } catch (error) {
+    console.error('Error getting cooks');
   }
+}
 
 function* feedSaga() {
-    yield takeLatest('FETCH_COOKS', fetchCooks);
+  yield takeLatest('FETCH_COOKS', fetchCooks);
 }
+
+export default feedSaga;

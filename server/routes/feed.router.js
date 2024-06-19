47bcb6e9f26232814a0bced5200f1685e718b0a1
Chris Harwood <cmharwood79@gmail.com>
2024-06-19 15:05:52 -0500

    refactoring

--- server/routes/feed.router.js
+++ server/routes/feed.router.js
@@ -3,11 +3,23 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 // GETS all the cooks for the cook list
+// GET route to fetch cooks with associated user profile image
 router.get('/', (req, res) => {
   const query = `
-    SELECT *
+    SELECT
+      cooks.id,
+      cooks.cook_name,
+      cooks.cook_date,
+      cooks.location,
+      cooks.recipe_notes,
+      cooks.cook_rating,
+      users.profile_image_url,
+      array_agg(cook_images.image_url) AS cook_images
     FROM "cooks"
-    ORDER BY "created_at" DESC;
+    JOIN "user" AS users ON cooks.user_id = users.id
+    LEFT JOIN "cook_images" ON cooks.id = cook_images.cook_id
+    GROUP BY cooks.id, users.id
+    ORDER BY cooks.created_at DESC;
   `;
   pool
     .query(query)
@@ -22,3 +34,24 @@ router.get('/', (req, res) => {
 
 //  ORDER BY "created_at" DESC;
 module.exports = router;
+
+// SELECT *
+//     FROM "cooks"
+//     ORDER BY "created_at" DESC;
+
+// This is the sql from the cooks router. Need to remove WHERE cooks.user_id = $1
+// SELECT
+//       cooks.id,
+//       cooks.cook_name,
+//       cooks.cook_date,
+//       cooks.location,
+//       cooks.recipe_notes,
+//       cooks.cook_rating,
+//       users.profile_image_url,
+//       array_agg(cook_images.image_url) AS cook_images
+//     FROM "cooks"
+//     JOIN "user" AS users ON cooks.user_id = users.id
+//     LEFT JOIN "cook_images" ON cooks.id = cook_images.cook_id
+//     WHERE cooks.user_id = $1
+//     GROUP BY cooks.id, users.id
+//     ORDER BY cooks.created_at DESC;

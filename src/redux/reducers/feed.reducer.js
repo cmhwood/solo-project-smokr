8b6e5a12930332207e438d8c91030204ec257cfa
Chris Harwood <cmharwood79@gmail.com>
2024-06-20 08:37:51 -0500

    refactor feed reducer

--- src/redux/reducers/feed.reducer.js
+++ src/redux/reducers/feed.reducer.js
@@ -1,9 +1,8 @@
 const feedReducer = (state = [], action) => {
-    if (action.type === 'SET_ALL_COOKS') {
-        return action.payload;
-    }
-    return state;
-
+  if (action.type === 'SET_ALL_COOKS') {
+    return action.payload;
+  }
+  return state;
 };
 
 export default feedReducer;

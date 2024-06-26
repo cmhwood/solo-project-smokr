fc4a355f1e100a67d481535d7ba1b5fd0f1829d7
Chris Harwood <cmharwood79@gmail.com>
2024-06-26 16:11:56 -0500

    added conditional render for users with no cooks

--- src/components/CooksPage/CooksPage.jsx
+++ src/components/CooksPage/CooksPage.jsx
@@ -17,62 +17,56 @@ const CooksPage = () => {
     history.push(`/cook/${cookId}`); // Navigate to cook details page
   };
 
+  const handleCreateNewCook = () => {
+    history.push('/cooks/new'); // Navigate to create new cook page
+  };
+
   return (
     <div className='container-fluid px-0'>
       <h2 className='my-4'>My Smokr</h2>
       <div className='row mx-0'>
-        {cooks.map((cook) => (
-          <div key={cook.id} className='col-12 px-0'>
-            <div className='card p-3 cook-card'>
-              <div className='d-flex align-items-center'>
-                <img
-                  src={cook.profile_image_url}
-                  alt='Profile'
-                  className='profile-image rounded-circle mr-3'
-                />
-                <div>
-                  <p className='mb-1'>
-                    {/* <strong>Cook Name: </strong> */}
-                    <span className='cook-name' onClick={() => handleCookClick(cook.id)}>
-                      {cook.cook_name}
-                    </span>
-                  </p>
-                  {/* <p>
-                  <strong>Cook Date:</strong> {cook.cook_date}
-                  </p> */}
-                  <p className='mb-1'>
-                    {/* <strong>Location:</strong>  */}
-                    {cook.location}
-                  </p>
+        {cooks.length === 0 ? (
+          <div className='text-center'>
+            <h2>What's on your Smokr?</h2>
+            <button className='new-btn-primary' onClick={handleCreateNewCook}>
+              Create a New Cook
+            </button>
+          </div>
+        ) : (
+          cooks.map((cook) => (
+            <div key={cook.id} className='col-12 px-0'>
+              <div className='card p-3 cook-card'>
+                <div className='d-flex align-items-center'>
+                  <img
+                    src={cook.profile_image_url}
+                    alt='Profile'
+                    className='profile-image rounded-circle mr-3'
+                  />
+                  <div>
+                    <p className='mb-1'>
+                      <span className='cook-name' onClick={() => handleCookClick(cook.id)}>
+                        {cook.cook_name}
+                      </span>
+                    </p>
+                    <p className='mb-1'>{cook.location}</p>
+                  </div>
+                </div>
+                <div className='cook-images'>
+                  {cook.cook_images?.slice(0, 2).map((url, index) => (
+                    <img
+                      key={index}
+                      src={url}
+                      alt={`Cook Image ${index}`}
+                      className={`cook-image ${
+                        index === 0 ? 'cook-image-large' : 'cook-image-large'
+                      }`}
+                    />
+                  ))}
                 </div>
               </div>
-              {/* <div className='mt-3'> */}
-              {/* Not showing rating on cooks page */}
-              {/* <p> 
-                  <strong>Rating:</strong> {cook.cook_rating}
-                </p> */}
-              {/* <div> */}
-              <div className='cook-images'>
-                {/* <strong>Cook Images:</strong> */}
-                {cook.cook_images?.slice(0, 2).map((url, index) => (
-                  <img
-                    key={index}
-                    src={url}
-                    alt={`Cook Image ${index}`}
-                    className={`cook-image ${
-                      index === 0 ? 'cook-image-large' : 'cook-image-large'
-                    }`}
-                  />
-                ))}
-                {/* </div> */}
-              </div>
-              {/* <p>
-                <strong>Recipe Notes:</strong> {cook.recipe_notes}
-                </p> */}
-              {/* </div> */}
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
